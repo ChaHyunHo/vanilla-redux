a5e8575ec2b78db7973fd5604f967bde6db46107
chahyeonho <chamym@naver.com>
2021-06-05 22:56:03 +0900

    init commit

--- valnilla-redux/index.js
+++ valnilla-redux/index.js
@@ -1,18 +1,18 @@
 const { createStore } = require("redux");
 
-const divToggle = document.querySelector('.toggle');
-const counter = document.querySelector('h1');
-const btnIncrease = document.querySelector('#increase');
-const btnDecrease = document.querySelector('#decrease');
+const divToggle     = document.querySelector('.toggle');
+const counter       = document.querySelector('h1');
+const btnIncrease   = document.querySelector('#increase');
+const btnDecrease   = document.querySelector('#decrease');
 
 //액션 타입과 액션 생성 함수 정의
 const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
-const INCREASE = 'INCREASE';
-const DECREASE = 'DECREASE';
+const INCREASE      = 'INCREASE';
+const DECREASE      = 'DECREASE';
 
-const toggleSwitch = () => ({type: TOGGLE_SWITCH});
-const increase = (difference) => ({ type: INCREASE, difference });
-const decrease = () => ({type:DECREASE});
+const toggleSwitch  = () => ({type: TOGGLE_SWITCH});
+const increase      = (difference) => ({ type: INCREASE, difference });
+const decrease      = () => ({type:DECREASE});
 
 
 //초기값 설정
